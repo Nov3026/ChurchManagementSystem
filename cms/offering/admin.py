c1f3f562a57c23ab64dd1e27fddfe8222d15b482
Nov3026 <alfredglaygbo92@gmail.com>
2024-12-10 15:04:06 +0000

    updated the offering models

--- cms/offering/admin.py
+++ cms/offering/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Offering
 
 # Register your models here.
+@admin.register(Offering)
+
+class OfferingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'offering_type', 'lrd', 'usd', 'description')
+    list_filter = ('offering_type',)
+    search_fields = ('offering_type','description')
